d84146f2e1f89a3cac9212927acc6c5851ade69b
Jason Liu <jason@jxnl.co>
2024-01-15 20:27:16 -0500

    remove tel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,13 +42,7 @@ export default function Page() {
                   </Button>
                 </a>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <Button className="h-8 w-8" variant="outline" size="icon">
-                    <PhoneIcon className="h-4 w-4" />
-                  </Button>
-                </a>
-              ) : null}
+          
               {RESUME_DATA.contact.social.map((social) => (
                 <Button
                   key={social.name}
@@ -66,11 +60,6 @@ export default function Page() {
               {RESUME_DATA.contact.email ? (
                 <a href={`mailto:${RESUME_DATA.contact.email}`}>
                   <span className="underline">{RESUME_DATA.contact.email}</span>
-                </a>
-              ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
                 </a>
               ) : null}
             </div>
